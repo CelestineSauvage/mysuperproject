3fabbab16a57f3346ce73955c596ca5dd06af4e0
CelestineSauvage <celestine.sauvage@protonmail.com>
2024-03-17 20:49:33 +0100

    add doc and change script

--- src/apiDataCollection/apiCallers/FranceEmploiApiCaller.py
+++ src/apiDataCollection/apiCallers/FranceEmploiApiCaller.py
@@ -6,6 +6,8 @@ import sys
 from pathlib import Path
 
 FRANCE_TRAVAIL_FILE_NAME = "FRANCE_TRAVAIL_API"
+ACCEPTED_CRITERAS = [
+    "departement, publieeDepuis, maxCreationDate, minCreationData"]
 JSON_KEYS = {  # (key,value) : (our keys, france_travail keys)
     "technical_id": "id",
     "place": "lieuTravail",
@@ -22,10 +24,15 @@ JSON_KEYS = {  # (key,value) : (our keys, france_travail keys)
 
 
 class FranceEmploiApiCaller:
-    # Class for calling the France Emploi APIs with OAth2 authentication
-    # Documentation here :
-    # - Workflow to follow to use the France Travail Connect APIs : https://francetravail.io/data/documentation/comprendre-dispositif-pole-emploi-connect/open-id-connect
-    # - APIs catalog : https://francetravail.io/data/api
+    """Class for calling the France Emploi APIs with OAth2 authentication
+    Documentation here :
+    Workflow to follow to use the France Travail Connect APIs : https://francetravail.io/data/documentation/comprendre-dispositif-pole-emploi-connect/open-id-connect
+    APIs catalog : https://francetravail.io/data/api
+
+    Raises:
+        UnauthorizedException: if the request return 400 or 401
+
+    """
 
     def __init__(self, client_id: str, client_secret: str):
         self.client_id = client_id
@@ -36,10 +43,10 @@ class FranceEmploiApiCaller:
         self.access_token = ""
 
     def authenticate(self, scope: str, params: dict = {}):
-        """_summary_
+        """get access_token to request France_Travail api
 
         Args:
-            scope (str): _description_
+            scope (str): specified scope from France_Travail api (in our case : api_offresdemploiv2 o2dsoffre)
             params (dict, optional): _description_. Defaults to {}.
         """
         # Function to get an access token via OAuth2 Grant Type
@@ -51,16 +58,16 @@ class FranceEmploiApiCaller:
         )
 
     def get_jobs_by_criterias(self, criteres: dict = {}) -> dict:
-        """_summary_
+        """get jobs by specified criterias
 
         Args:
-            criteres (dict, optional): _description_. Defaults to {}.
+            criteres (dict, optional): list of criterias. Defaults to {}.
 
         Raises:
-            UnauthorizedException: _description_
+            UnauthorizedException: exit if HTTP response is 400 or 401
 
         Returns:
-            dict: _description_
+            dict: jobs
         """
         # Function to get jobs list by criterias
         # Documentation here :
@@ -77,7 +84,7 @@ class FranceEmploiApiCaller:
 
         # if Unauthorized response, raise exception
         if (response.status_code == 401 or response.status_code == 400):
-            raise UnauthorizedException(response.message)
+            raise UnauthorizedException(response.content)
 
         return response
 
@@ -101,16 +108,23 @@ class DepartmentJobsCaller:
             maxCreationDate (string) : format yyyy-MM-dd'T'hh:mm:ss'Z'
             minCreationDate (string) : format yyyy-MM-dd'T'hh:mm:ss'Z')
         """
+        assert (key in ACCEPTED_CRITERAS for key in optionnals.keys())
         self.FranceEmploiApiCaller = FranceEmploiApiCaller
         self.criteras = optionnals
         self.range_min = 0
         self.range_max = 149
-        f_name = self.__create_file_name(self.criteras["departement"], path)
+        f_name = self.__create_file_name(self.criteras["departement"])
         self.json_file_path = Path(path) / f"{f_name}.json"
 
-    def __create_file_name(self, department, path):
-        """
-        Create file name based on actual day : format Y_m_d_H_M_S_dep_FRANCE_TRAVAIL_API.json
+    def __create_file_name(self, department: str) -> str:
+        """Create file name based on actual day :
+        format Y_m_d_H_M_S_dep_FRANCE_TRAVAIL_API.json
+
+        Args:
+            department (_type_): department number
+
+        Returns:
+            str: file name
         """
         now = datetime.datetime.now()
         self.dt_string = now.strftime("%Y_%m_%d_%H_%M_%S")
@@ -118,46 +132,65 @@ class DepartmentJobsCaller:
             department}_{self.dt_string}"
         return file_name
 
-    def __retrieve_number_of_jobs(self, header: dict):
-        """
-        Retrieve number of jobs for one departement. This information is store
+    def __retrieve_number_of_jobs(self, header: dict) -> int:
+        """ Retrieve number of jobs for one departement. This information is store
         in the header of HTTP response for GET request as
         'Content-Range': 'offres 0-149/3430', value is after /
+
+        Args:
+            header (dict): header of HTTP response
+
+        Returns:
+            int: total of jobs for specifics criteras
         """
         to_parse = header["Content-Range"]
         number_of_jobs = to_parse.split('/')[1]
         return int(number_of_jobs)
 
-    def __store_value(self, result: list, key: str):
+    def __store_value(self, result: dict, key: str):
+        """Store value of the result["key"] if exist, else store None
+
+        Args:
+            result (dict): job in json format
+            key (str): key
+
+        Returns:
+            _type_: Value or None
+        """
         try:
             return result[key]
         except KeyError:
             return None
 
-    def __store_json(self, json_data, f):
+    def __store_json(self, json_data: dict, todump: list):
+        """Store json_data into the file self.json_file_path
+
+        Args:
+            json_data (dict): job in json format
+            todump (list): all jobs stored in a list
         """
-        Store json_data into the file self.json_file_path
-        TODO : save only attributes needed
-        """
+
         for res in json_data:
             cleaned_data = {}
             for (j_key, france_travail_key) in JSON_KEYS.items():
                 cleaned_data[j_key] = self.__store_value(
                     res, france_travail_key)
-            json.dump(cleaned_data, f)
+            todump.append(cleaned_data)
 
     def get_jobs_by_department(self):
         """
-        Function to get all jobs for one specific departement
+        get all jobs based on the criteras passed on the creation of the instance
         """
 
         json_file = open(self.json_file_path, "w")
         total = 150
         json_response = ""
+        json_todump = []
 
-        # tant qu'on n'a pas reçu tous les jobs
+        # while all jobs not retrieve but < 3000 range
         while self.range_max < (min(total, 3000)):
 
+            # update new range of jobs to download
             self.criteras["range"] = f"{
                 str(self.range_min)}-{str(self.range_max)}"
 
@@ -170,20 +203,13 @@ class DepartmentJobsCaller:
 
             total = self.__retrieve_number_of_jobs(response.headers) - 1
             json_response = json.loads(response.text)
-            self.__store_json(json_response["resultats"], json_file)
+            self.__store_json(json_response["resultats"], json_todump)
             self.range_min = self.range_max + 1
             self.range_max = min(self.range_max+150, total)
 
         self.criteras.pop("range")
-        metadata = {"request": self.criteras,
-                    "date": self.dt_string}
-        json.dump({"metadata": metadata}, json_file)
+        data = {"results": json_todump,
+                "metadata": {"request": self.criteras,
+                             "date": self.dt_string}}
+        json.dump(data, json_file, indent=4)
         json_file.close()
-
-
-def main():
-    print("Hello World!")
-
-
-if __name__ == "__main__":
-    main()
--- src/francetravail_download.py
+++ src/francetravail_download.py
@@ -1,37 +1,77 @@
-import yaml
+import sys
+import os
+import requests
+import argparse
 from pathlib import Path
 from apiDataCollection.apiCallers.FranceEmploiApiCaller import \
     FranceEmploiApiCaller, DepartmentJobsCaller
 
 
-# Load credentials of API providers
-with open(Path("./api_providers_credentials.yml"), "r") as ymlfile:
-    credentials = yaml.safe_load(ymlfile)
-france_emploi_client_id = credentials["france_emploi"]["client_id"]
-france_emploi_client_secret = credentials["france_emploi"]["client_secret"]
+def collect_from_france_emploi():
+    france_emploi_client_id = os.environ.get("FRANCE_EMPLOI_CLIENT_ID", "")
+    france_emploi_client_secret = os.environ.get(
+        "FRANCE_EMPLOI_CLIENT_SECRET", "")
+    print("Start of data collection for France Emploi")
+    # Initialize the France Emploi API caller
+    franceEmploi = FranceEmploiApiCaller(
+        france_emploi_client_id, france_emploi_client_secret)
 
-# Initialize the France Emploi API caller
-franceEmploi = FranceEmploiApiCaller(
-    france_emploi_client_id, france_emploi_client_secret)
+    # Authenticate to the France Emploi API services
+    franceEmploi.authenticate("api_offresdemploiv2 o2dsoffre", {
+        "realm": "/partenaire"})
 
-# Authenticate to the France Emploi API services
-franceEmploi.authenticate("api_offresdemploiv2 o2dsoffre", {
-                          "realm": "/partenaire"})
+    return franceEmploi
 
-response = franceEmploi.get_jobs_by_criterias({"departement": "30"})
 
-print(response.headers)
+def download(kwargs):
+    # Load credentials of API providers
+    # TODO : change with DataCollector
+    france_emploi = collect_from_france_emploi()
 
-directory = "/home/celestine/smart_emploi_api/downloads/FranceTravail_15_03_2024"
-Path(directory).mkdir(parents=True, exist_ok=True)
-int_departments = list(range(1, 96, 1))
-# int_departments.remove(20)
-departments = [(f"{number:02d}") for number in int_departments]
-departments += ["971", "972", "973", "974", "976"]
-for dep in departments:
-    departement_download = DepartmentJobsCaller(
-        franceEmploi,
-        path=directory,
-        departement=dep,
-        publieeDepuis=7)
-    departement_download.get_jobs_by_department()
+    directory = kwargs["path"]
+    # create directory if doesn't exist
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    if (kwargs["department"] is None):  # if department not specified
+        int_departments = list(range(1, 96, 1))
+        int_departments.remove(20)
+        departments = [(f"{number:02d}") for number in int_departments]
+        departments += ["971", "972", "973", "974", "976"]
+    else:
+        departments = [kwargs["department"]]
+
+    kwargs.pop("department")
+    kwargs.pop("path")
+
+    try:
+        for dep in departments:
+            departement_download = DepartmentJobsCaller(
+                france_emploi,
+                path=directory,
+                departement=dep,
+                **kwargs)
+            try:
+                departement_download.get_jobs_by_department()
+            except requests.exceptions.RequestException:
+                pass
+    except KeyboardInterrupt:  # SIG INT
+        sys.exit()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Download jobs from France API in json files')
+    parser.add_argument('path', type=str,
+                        help='directory where json files will be store')
+    parser.add_argument('--department',
+                        help='specified department, if not, all departments are dl')
+    parser.add_argument('--publieDepuis',
+                        help="1, 3, 7, 14 or 31 last day(s)")
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+    download(args_dict)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
