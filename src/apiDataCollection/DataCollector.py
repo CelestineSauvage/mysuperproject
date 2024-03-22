84c82c12e23edd96f21786aa453cb1f477869b2b
ELASTRUCCI <manulastrucci@yahoo.fr>
2024-03-22 15:42:58 +0100

    connection with adzunaa

--- src/apiDataCollection/DataCollector.py
+++ src/apiDataCollection/DataCollector.py
@@ -1,6 +1,13 @@
 from apiDataCollection.apiCallers.FranceEmploiApiCaller import FranceEmploiApiCaller
 from apiDataCollection.apiCallers.MuseApiCaller import MuseApiCaller
+from apiDataCollection.apiCallers.AdzunaApiCaller import AdzunaApiCaller
+import requests
+import pandas as pd
 import os
+from math import *
+import time
+import json
+
 
 class DataCollector:
     ### Class for collecting the data from their sources
@@ -15,18 +22,24 @@ class DataCollector:
         france_emploi_client_id = os.environ.get("FRANCE_EMPLOI_CLIENT_ID", "")
         france_emploi_client_secret = os.environ.get("FRANCE_EMPLOI_CLIENT_SECRET", "")
         muse_client_secret = os.environ.get("MUSE_CLIENT_SECRET", "")
+        adzuna_client_id = os.environ.get("ADZUNA_API_ID", "")
+        adzuna_client_secret = os.environ.get("ADZUNA_API_SECRET", "")
+
         if france_emploi_client_id == "":
             raise CredentialNotFoundException("france_emploi_client_id environment variable not found")
         if france_emploi_client_secret == "":
             raise CredentialNotFoundException("france_emploi_client_secret environment variable not found")
         if muse_client_secret == "":
             raise CredentialNotFoundException("muse_client_secret environment variable not found")
+        if adzuna_client_id == "":
+            raise CredentialNotFoundException("adzuna_client_id environment variable not found")
+        if adzuna_client_secret == "":
+            raise CredentialNotFoundException("adzuna_client_secret environment variable not found")
     
         DataCollector.__collectFromFranceEmploi(france_emploi_client_id, france_emploi_client_secret)
         DataCollector.__collectFromMuse(muse_client_secret)
+        DataCollector.__collectFromAdzuna(adzuna_client_id, adzuna_client_secret)
         
-        print("Start of the step data collection")
-
     @staticmethod
     def __collectFromFranceEmploi(client_id: str, client_secret: str):
         print("Start of data collection for France Emploi")
@@ -60,6 +73,63 @@ class DataCollector:
             list_all_jobs_parsed = list_all_jobs_parsed + jobs_parsed_list
 
         print("End of data collection for Muse")
+
+    @staticmethod
+    def __collectFromAdzuna(client_id: str, client_secret: str):
         
+        Adzuna = AdzunaApiCaller(client_id, client_secret)
+        
+        country = 'fr'
+        page = 1
+        critere = {
+            'results_per_page': '50',
+            'sort_by': 'date',
+            'max_days_old': '1',
+        }
+
+        adzuna_jobs = Adzuna.get_jobs_by_criterias(country=country, page=page, criteres=critere)
+
+        #    suite job
+        # r = requests.get(f'https://api.adzuna.com/v1/api/jobs/fr/search/1', params=query)
+        # response = r.json()
+        # nb_count = response['count']
+        # nb_page = ceil(nb_count/50)
+
+        # keys_value = set()
+            
+        # start_time = time.time()
+        # all_jobs = []
+        # for page in range(1,2000):
+        #     print(page)
+        #     r = requests.get(f'http://api.adzuna.com/v1/api/jobs/fr/search/{page}', params=query)
+        #     response = r.json()
+        #     all_jobs.extend(response['results'])
+
+        #     with open('all_jobs_22mars.json', 'w') as f:
+        #         json.dump(all_jobs, f)
+        
+        #     df = pd.DataFrame(all_jobs)
+        #     df.to_csv("all_jobs_22mars.csv")
+                  
+        # print(len(all_jobs))
+
+        # with open('all_jobs.json', 'w') as f:
+        #     json.dump(all_jobs, f)
+        
+        # df = pd.DataFrame(all_jobs)
+        # df.to_csv("all_jobs.csv")
+        
+        # print("Start of the step data collection")
+
+        # ['__CLASS__', 'adref', 'category', 'company', 'contract_time', 'contract_type', 'created', 'description', 'id', 'latitude', 'location', 'longitude', 'redirect_url', 'salary_is_predicted', 'salary_max', 'salary_min', 'title']
+
+        # query = {'app_id': adzuna_client_id,
+        #     'app_key': adzuna_client_secret,
+        #     'content-type': 'application/json',
+        #     'results_per_page': '50', # max 50
+        #     'sort_by': 'date',
+        #     'max_days_old': '1',
+        # }
+ 
 class CredentialNotFoundException(Exception):
     pass
